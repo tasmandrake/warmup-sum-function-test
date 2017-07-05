7c6404e5e4df904ef0a4708164d2df2fc7186599
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-07-05 14:18:37 -0600

    Added solution

--- sum.js
+++ sum.js
@@ -1,5 +1,21 @@
-function sum() {
-
+function sum(arr) {
+  if (!arr) {
+    return 0;
+  } else if (!Array.isArray(arr)) {
+    return 'Input should be an array of numbers';
+  } else if (!arr.length) {
+    return 0;
+  } else if (Array.isArray(arr)) {
+    return arr.reduce((a,b) => {
+      if (typeof b !== 'number' || typeof a !== 'number') {
+        return 'Input should be an array of numbers';
+      } else {
+        return a + b;
+      }
+    });
+  } else {
+    return 'Input should be an array of numbers';
+  }
 }
 
 module.exports = sum;
